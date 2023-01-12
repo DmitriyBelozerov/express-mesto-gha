9efd247928043f55e3362c93f834d8226fd21e45
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-01-12 15:07:46 +0300

    12/01/23 2

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,8 +4,15 @@ const Card = require('../models/card');
 const getCards = (req, res) => {
   Card.find({})
     // .populate('owner')
-    .then((cards) => res.status(200).send(cards))
-    .catch((err) => res.status(400).send(err))
+    .then(
+      (cards) => {
+        if (!cards) {
+          res.status(404).send({ message: 'Карточки не найдены' })
+        } else {
+          res.status(200).send({ data: cards })
+        }
+      })
+    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }))
 }
 
 //ok
@@ -14,33 +21,46 @@ const createCard = (req, res) => {
   console.log({ name, link, owner: req.user._id });
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
-      res.status(200).send(card);
+      res.status(200).send({ data: card });
     })
-    .catch((err) => res.status(500).send(err))
+    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }))
 }
 //ok
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) =>
-      res.status(200).send(card)
-    )
-    .catch((err) => res.status(500).send(err))
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' })
+      }
+      res.status(200).send({ data: card })
+    })
+
+
+    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }))
 }
 //ok
 const likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) =>
-      res.status(200).send(card)
-    )
-    .catch((err) => res.status(500).send(err))
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' })
+      } else {
+        res.status(200).send({ data: card })
+      }
+    })
+    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }))
 }
 //ok
 const deletelikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) =>
-      res.status(200).send(card)
-    )
-    .catch((err) => res.status(500).send(err))
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Карточка не найдена' })
+      } else {
+        res.status(200).send({ data: card })
+      }
+    })
+    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }))
 }
 
 module.exports = { createCard, getCards, deleteCard, likeCard, deletelikeCard };
